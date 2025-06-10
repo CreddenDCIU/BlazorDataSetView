6a3f7b635d27783307f48b12c6ff21c691438b46
CreddenDCIU <credden@dciu.org>
2025-06-10 14:53:49 -0400

    First pass at implementing editable fields.

--- BlazorDataSetView/Interfaces/IViewDefinitionProvider.cs
+++ BlazorDataSetView/Interfaces/IViewDefinitionProvider.cs
@@ -1,8 +1,11 @@
-﻿namespace BlazorDataSetView.Interfaces;
+﻿using BlazorDataSetView.Enums;
+
+namespace BlazorDataSetView.Interfaces;
 
 public interface IViewDefinitionProvider
 {
-    public Dictionary<string, (bool Visible, int DisplayOrder)> GetDisplayedFieldDictionary<TEntity>() where TEntity : class;
+    public Dictionary<string, (bool Visible, EditableInputTypes EditableType, int DisplayOrder)> GetDisplayedFieldDictionary<TEntity>() where TEntity : class;
     public Dictionary<string, (bool Visible, object InitialValue)> GetFilterableFieldDictionary<TEntity>() where TEntity : class;
     public Func<TEntity?, string?, string> GetFieldFormatterFunc<TEntity>() where TEntity : class;
+    public Func<string?, IEnumerable<KeyValuePair<string, object>>?> GetPossibleValuesForField<TEntity>() where TEntity : class;
 }
